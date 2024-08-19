362a7c5856d32ac3ee97499c8127c7b24ab6c3dc
Rakesh-TN <tnrakesh68@gmail.com>
2024-08-19 22:25:20 +0530

    New Learning

--- JS/Script.js
+++ JS/Script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $('#treat').text("Hello World")
-    $('body').css('background-color', 'Black')
-    $('body').css('color', 'White')
+    $('.light').click(function(){
+        $('body').css('background-color', 'WHITE');
+        $('body').css('color', 'black')
+    }).text('Light');   
+    $('.dark').click(function(){
+        $('body').css('background-color', 'Black')
+        $('body').css('color', 'White')
+    }).text('Dark');
+    
 })
