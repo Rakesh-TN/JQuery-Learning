fe0c305798ba1d36016b1f8eda452a2c88eb8dd0
Rakesh-TN <tnrakesh68@outlook.com>
2024-08-19 23:01:17 +0530

    Add Bootstrap

--- JS/Script.js
+++ JS/Script.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-    $('#treat').text("Hello World")
+    $('main').addClass('d-flex flex-column justify-content-center align-items-center min-vh-100')
+    $('#treat').text("Hello World");
     $('.light').click(function(){
         $('body').css('background-color', 'WHITE');
         $('body').css('color', 'black')
-    }).text('Light');   
+    }).text('Light').addClass('btn btn-light');   
     $('.dark').click(function(){
         $('body').css('background-color', 'Black')
         $('body').css('color', 'White')
-    }).text('Dark');
-    
+    }).text('Dark').addClass('btn btn-dark');;
+    $("#alert-btn").click(function(){
+        $('#alert').html("<div class='alert alert-danger' role='alert'>Code Red!</div>")
+    }).text('Alert').addClass('m-1 btn btn-danger')
+
 })
